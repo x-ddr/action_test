daf9c1ce4d58674d61575327bf88cfcffad08340
xd <xd@x.local>
2021-02-18 22:09:00 +0800

    IQIYI

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ Wps_id = os.environ['WPS_KEY']
 
 
 print(Wps_id)
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# 打开文件
+fo = open("test.txt", "w")
+str = Wps_id
+fo.write( str )
+
+# 关闭文件
+fo.close()
